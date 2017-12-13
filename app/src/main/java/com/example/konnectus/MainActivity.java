dd18c76ea6555c9ef4c967dae2920acf8ef476f0
Ana Adriana Gincu <ana_gincu@hotmail.com>
2017-12-13 08:29:04 +0100

    relevant comments

--- app/src/main/java/com/example/konnectus/MainActivity.java
+++ app/src/main/java/com/example/konnectus/MainActivity.java
@@ -16,7 +16,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
-
+    // block for declaring  instance variables
     private ArrayList<Song> songList;
     private ListView songView;
     //declared new instance for the MusicService
@@ -28,12 +28,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+       //storing the songs in a list and display them in the ListView instance in the main layout --
+        //-- retrieving the ListView instance using the ID we gave it in the main layout
         songView = (ListView)findViewById(R.id.song_list);
+        //Instantiate the list
         songList = new ArrayList<Song>();
 
-        getSongList();
 
+        getSongList();
+        //  display the song -- sort the data so that the songs are presented alphabetically --
+        // display the song --  --by use the title variable in the Song class, using the get methods we added
+        // display the song --  --to implement a compare method, sorting the songs by title
         Collections.sort(songList, new Comparator<Song>(){
             public int compare(Song a, Song b){
                 return a.getTitle().compareTo(b.getTitle());
@@ -104,12 +109,17 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+     // helper method to retrieve the audio file information
     public void getSongList() {
         //retrieve song info
+        // creating ContentResolver instance, retrieve the URI for external music files --
+        // --creating a Cursor instance using the ContentResolver instance to query the music files
         ContentResolver musicResolver = getContentResolver();
         Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
-
+        // iterating over the results, first checking for valid data
+        //first retrieve the column indexes for the data items that we are interested in for each song
+        // then  use these to create a new Song object and add it to the list, before continuing to loop through the results
         if(musicCursor!=null && musicCursor.moveToFirst()){
             //get columns
             int titleColumn = musicCursor.getColumnIndex
